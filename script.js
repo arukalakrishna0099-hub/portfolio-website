c58381fdfa61facffb6ce25d1b3345d6316220e6
Krishna Arukala <arukala.krishna0099@gmail.com>
2026-06-02 20:52:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+const resumeBtns=document.querySelectorAll('.resume-btn');
+
+
+
+resumeBtns.forEach((btn,idx)=>{
+    btn.addEventListener('click',()=>{
+        const resumeDetails=document.querySelectorAll('.resume-detail');
+
+        resumeBtns.forEach(btn=>{
+            btn.classList.remove('active');
+        });
+        btn.classList.add('active');
+
+        resumeDetails.forEach(detail=>{
+            detail.classList.remove('active');
+        });
+        resumeDetails[idx].classList.add('active');
+    });
+}); 
+
+
+
+const arrowRight=document.querySelector('.project-box .navigation .arrow-right');
+const arrowLeft=document.querySelector('.project-box .navigation .arrow-left');
+
+
+
+let index= 0;
+
+
+
+const activeproject=() => {
+    const imgSlide=document.querySelector('.project-carousel .img-slide');
+    const projectDetails=document.querySelectorAll('.project-detail');
+    imgSlide.style.transform = `translateX(-${index * 100}%)`;
+
+    projectDetails.forEach(detail =>{
+        detail.classList.remove('active')
+    });
+    projectDetails[index].classList.add('active');
+}
+
+
+arrowRight.addEventListener('click',()=>{
+    if(index<4){
+        index++;
+        arrowLeft.classList.remove('disabled')
+    }else{
+        index=5;
+        arrowRight.classList.add('disabled');
+    }
+
+    activeproject();
+});
+
+
+
+arrowLeft.addEventListener('click',()=>{
+    if(index>1){
+        index--;
+        arrowRight.classList.remove('disabled');
+    }
+    else{
+        index=0;
+        arrowLeft.classList.add('disabled');
+    }
+
+    activeproject();
+});
